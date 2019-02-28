ae49fd26dd359f68beecfbbe69fa31e3d1f56f7b
MHamilton <32247409+cyberboy379@users.noreply.github.com>
2019-02-28 09:57:55 +0000

    Improved map
    Uploaded wrong files for last patch
    Includes:
    -Difficulty
    -Scrolling
    -GetPath function

--- Map.cpp
+++ Map.cpp
@@ -40,76 +40,212 @@ bool Map::GeneratePath(int Difficulty)
 
 
 	m_Path.clear();
-
-
-	//Continues randomly generating path until it has reached the bottom of the screen
-	while (position < m_Width * m_Height - m_Width)
+	if (Difficulty == 0)
 	{
-		//Creating a value to be changed once a valid direction for the path to move has been selected
-		bool Continue{ false };
-		while (!Continue)
+		while (position < m_Width * m_Height - m_Width)
 		{
-			//Switch case to randomly select the direction for the path to go
-			switch (rand() % 7)
+			//Creating a value to be changed once a valid direction for the path to move has been selected
+			bool Continue{ false };
+			while (!Continue)
 			{
-				//cases 0-2 make the path to go right
-			case 0:
-
-			case 1:
-
-			case 2:
-				if (lastDirection != 'r' && (position % m_Width) != 0) {
-					pointer += Left;
-					position += Left;
-					lastDirection = 'l';
-					*pointer = 1;
-					Continue = true;
-				}
-				break;
-				//cases 3-5 make the path go left
-			case 3:
-
-			case 4:
-
-			case 5:
-				if (lastDirection != 'l' && (position % m_Width) != m_Width - 1) {
-					pointer += Right;
-					position += Right;
-					lastDirection = 'r';
-					*pointer = 1;
-					Continue = true;
-				}
-				break;
-				//case 6 makes the path go down
-			case 6:
-				pointer += Down;
-				position += Down;
-				lastDirection = 'd';
-				*pointer = 1;
-				if (position < m_Width* m_Height - m_Width)
+				//Switch case to randomly select the direction for the path to go
+				switch (rand() % 4)
 				{
-					m_Path.push_back(HAPISPACE::VectorI(position % m_Width, (position - (position % m_Width)) / m_Width));
+				//cases 0-2 make the path to go right
+				case 0:
+
+					if (lastDirection != 'r') 
+					{
+						while ((position % m_Width) > 1)
+						{
+							pointer += Left;
+							position += Left;
+							lastDirection = 'l';
+							*pointer = 1;
+							m_Path.push_back(HAPISPACE::VectorI(position % m_Width, (position - (position % m_Width)) / m_Width));
+							Continue = true;
+						}
+					}
+					break;
+					//cases 3-5 make the path go left
+
+				case 1:
+					if (lastDirection != 'l')
+					{
+						while ((position % m_Width) < m_Width - 2)
+						{
+							pointer += Right;
+							position += Right;
+							lastDirection = 'r';
+							*pointer = 1;
+							m_Path.push_back(HAPISPACE::VectorI(position % m_Width, (position - (position % m_Width)) / m_Width));
+							Continue = true;
+						}
+					}
+					break;
+					//case 6 makes the path go down
+				case 2:
+
+				case 3:
 					pointer += Down;
 					position += Down;
+					lastDirection = 'd';
 					*pointer = 1;
+					if (position < m_Width* m_Height - m_Width)
+					{
+						m_Path.push_back(HAPISPACE::VectorI(position % m_Width, (position - (position % m_Width)) / m_Width));
+						pointer += Down;
+						position += Down;
+						*pointer = 1;
+					}
+					Continue = true;
+					break;
+				default:
+					return false;
+					break;
 				}
-				Continue = true;
-				break;
-			default:
-				return false;
-				break;
 			}
 		}
-		m_Path.push_back(HAPISPACE::VectorI(position % m_Width, (position - (position % m_Width)) / m_Width));
+	}
+	else if(Difficulty == 1)
+	{
+	//Continues randomly generating path until it has reached the bottom of the screen
+		while (position < m_Width * m_Height - m_Width)
+		{
+		//Creating a value to be changed once a valid direction for the path to move has been selected
+		
+			bool Continue{ false };
+			while (!Continue)
+			{
+				//Switch case to randomly select the direction for the path to go
+				switch (rand() % 7)
+				{
+					//cases 0-2 make the path to go right
+				case 0:
+
+				case 1:
+
+				case 2:
+					if (lastDirection != 'r' && (position % m_Width) > 1) {
+						pointer += Left;
+						position += Left;
+						lastDirection = 'l';
+						*pointer = 1;
+						Continue = true;
+					}
+					break;
+					//cases 3-5 make the path go left
+				case 3:
+
+				case 4:
+
+				case 5:
+					if (lastDirection != 'l' && (position % m_Width) < m_Width - 2) {
+						pointer += Right;
+						position += Right;
+						lastDirection = 'r';
+						*pointer = 1;
+						Continue = true;
+					}
+					break;
+					//case 6 makes the path go down
+				case 6:
+					pointer += Down;
+					position += Down;
+					lastDirection = 'd';
+					*pointer = 1;
+					if (position < m_Width* m_Height - m_Width)
+					{
+						m_Path.push_back(HAPISPACE::VectorI(position % m_Width, (position - (position % m_Width)) / m_Width));
+						pointer += Down;
+						position += Down;
+						*pointer = 1;
+					}
+					Continue = true;
+					break;
+				default:
+					return false;
+					break;
+				}
+			}
+			m_Path.push_back(HAPISPACE::VectorI(position % m_Width, (position - (position % m_Width)) / m_Width));
+		}
+	}
+	else
+	{
+		while (position < m_Width * m_Height - m_Width)
+		{
+		//Creating a value to be changed once a valid direction for the path to move has been selected
+		
+			bool Continue{ false };
+			while (!Continue)
+			{
+				//Switch case to randomly select the direction for the path to go
+				switch (rand() % 7)
+				{
+				case 0:
+					//case 0 makes the path to go right
+					if (lastDirection != 'r' && (position % m_Width) > 1) {
+						pointer += Left;
+						position += Left;
+						lastDirection = 'l';
+						*pointer = 1;
+						Continue = true;
+					}
+					break;
+				case 1:
+					//case 1 makes the path to go right
+					if (lastDirection != 'l' && (position % m_Width) < m_Width - 2) {
+						pointer += Right;
+						position += Right;
+						lastDirection = 'r';
+						*pointer = 1;
+						Continue = true;
+					}
+					break;
+				case 2:
+					//cases 2-6 makes the path to go down
+				case 3:
+
+				case 4:
+
+				case 5:
+					
+				case 6:
+					pointer += Down;
+					position += Down;
+					lastDirection = 'd';
+					*pointer = 1;
+					if (position < m_Width* m_Height - m_Width)
+					{
+						m_Path.push_back(HAPISPACE::VectorI(position % m_Width, (position - (position % m_Width)) / m_Width));
+						pointer += Down;
+						position += Down;
+						*pointer = 1;
+					}
+					Continue = true;
+					break;
+				default:
+					return false;
+					break;
+				}
+			}
+			m_Path.push_back(HAPISPACE::VectorI(position % m_Width, (position - (position % m_Width)) / m_Width));
+		}
+
 	}
 
 	return true;
 }
 
-void Map::RenderMap(int yOffset)
+void Map::RenderMap(int &yOffset)
 {
-	std::shared_ptr<HAPISPACE::Sprite> green = HAPI_Sprites.MakeSprite("Data\\thing.png",1);
-	std::shared_ptr<HAPISPACE::Sprite> pink = HAPI_Sprites.MakeSprite("Data\\thing2.png",1);
+	if (yOffset < 0)
+		yOffset = 0;
+	else if (yOffset > 40 * (m_Height - 20))
+		yOffset = 40 * (m_Height - 20);
+	std::shared_ptr<HAPISPACE::Sprite> green = HAPI_Sprites.MakeSprite("Data\\Grass.png");
+	std::shared_ptr<HAPISPACE::Sprite> pink = HAPI_Sprites.MakeSprite("Data\\Sand.png");
 	bool* pointer = m_MapData;
 	for (int i{ 0 }; i < m_Height; i++)
 	{
@@ -117,18 +253,22 @@ void Map::RenderMap(int yOffset)
 		{
 			if (*pointer == 0)
 			{
-				green->GetTransformComp().SetPosition({ (float)(j * 40), (float)(i * 40) });
+				green->GetTransformComp().SetPosition({ (float)(j * 40), (float)((i * 40) - yOffset) });
 				green->Render(SCREEN_SURFACE);
 				//HAPI_Sprites.RenderText(VectorI(j * 32, i * 40), Colour255::GREEN, std::to_string(*pointer), 20);
 				pointer++;
 			}
-			else
+			else if(*pointer == 1)
 			{
-				pink->GetTransformComp().SetPosition({ (float)(j * 40), (float)(i * 40) });
+				pink->GetTransformComp().SetPosition({ (float)(j * 40), (float)((i * 40) - yOffset) });
 				pink->Render(SCREEN_SURFACE);
 				//HAPI_Sprites.RenderText(VectorI(j * 32, i * 40), Colour255::MAGENTA, std::to_string(*pointer), 20);
 				pointer++;
 			}
 		}
 	}
+}
+
+void Map::SelectTile(const int & mouseX, const int & mouseY, const int & yOffset)
+{
 }
