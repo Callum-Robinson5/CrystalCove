087480b38c92d7c683fb1a52ac97dd01ef16db2e
natxtw <36550708+natxtw@users.noreply.github.com>
2019-03-28 10:05:02 +0000

    Add files via upload
    merge fix atmp3

--- EnemyAI.h
+++ EnemyAI.h
@@ -14,16 +14,19 @@ public:
 	void move(const std::vector<HAPISPACE::VectorF> &path);
 	void die();
 	bool isAlive();
+	void takeDamage(int Damage) { if(m_Alive)enemy_health -= Damage; };
 	void update(int &yOffset, const std::vector<HAPISPACE::VectorF> &path);
 
+	const std::shared_ptr<Sprite> getSprite() { return sprite; };
 	const VectorF getPosition() { return m_Position; };
+	const int getHealth() { return enemy_health; };
 
 private:
 	int m_Checkpoint = 0;
 	int m_speed{ 10 };
 	VectorF m_Position;
-	std::shared_ptr<Sprite> sprite = HAPI_Sprites.MakeSprite("Data\\Golem.png");
-	int enemy_health{10};
+	std::shared_ptr<Sprite> sprite = HAPI_Sprites.MakeSprite("Data\\Enemy\\Golem.png");
+	int enemy_health{1};
 	bool m_Alive{ false };
 	bool m_Died{ true };
 	XP* m_Xp;
